606b21db51481d913087c1f4bb9cce73f588687f
yueludanfeng <yueludanfeng@gmail.com>
2016-08-23 16:34:47 +0800

    login2

---
+++ .login2.sh
@@ -0,0 +1,4 @@
+scp -P 8792 lichenggui@172.16.55.95:/home/jinglun/os/hisi_v200_XXX/frameworks/base/core/java/com/routon/os/TProcess.java /home/lxm/new_task/study/
+scp -P 8792 
+用戶名：lichenggui
+密碼：e2008jl
